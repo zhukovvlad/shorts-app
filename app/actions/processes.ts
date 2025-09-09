1160ff8b47433b27910fea928bef055aa2c53d89
zhukovvlad <zhukovvlad81@gmail.com>
2025-09-09 16:13:57 +0300

    07: captions-created

--- app/actions/processes.ts
+++ app/actions/processes.ts
@@ -1,36 +1,43 @@
 import { prisma } from "../lib/db";
+import { videoDuration } from "../lib/duration";
 import { findPrompt } from "../lib/findPrompt";
+import { generateAudio } from "./audio";
+import { generateCaptions } from "./captions";
 import { generateImages } from "./image";
 import { generateScript } from "./script";
 
 export const processVideo = async (videoId: string) => {
   try {
-    // const prompt = await findPrompt(videoId);
-    // const script = await generateScript(prompt || "");
-    // const scriptData = JSON.parse(script || "");
-    // const contentTexts = scriptData.content.map(
-    //   (item: { contentText: string }) => item.contentText
-    // );
-    // const fullContent = contentTexts.join(" ");
-    // const imagePrompts = scriptData.content.map(
-    //   (item: { imagePrompt: string }) => item.imagePrompt
-    // );
+    const prompt = await findPrompt(videoId);
+    const script = await generateScript(prompt || "");
+    const scriptData = JSON.parse(script || "");
+    const contentTexts = scriptData.content.map(
+      (item: { contentText: string }) => item.contentText
+    );
+    const fullContent = contentTexts.join(" ");
+    const imagePrompts = scriptData.content.map(
+      (item: { imagePrompt: string }) => item.imagePrompt
+    );
 
-    // console.log("Generated Script Content Texts:", fullContent);
-    // console.log("Generated Image Prompts:", imagePrompts);
+    console.log("Generated Script Content Texts:", fullContent);
+    console.log("Generated Image Prompts:", imagePrompts);
 
-    // await prisma.video.update({
-    //   where: {
-    //     videoId,
-    //   },
-    //   data: {
-    //     content: fullContent,
-    //     imagePrompts: imagePrompts,
-    //   },
-    // });
+    await prisma.video.update({
+      where: {
+        videoId,
+      },
+      data: {
+        content: fullContent,
+        imagePrompts: imagePrompts,
+      },
+    });
 
-    await generateImages(videoId);
-  
+    const imagesPromise = generateImages(videoId);
+    await generateAudio(videoId);
+    await generateCaptions(videoId);
+    await imagesPromise;
+    await videoDuration(videoId);
+
   } catch (error) {
     console.error("Error processing video:", error);
     throw error;
