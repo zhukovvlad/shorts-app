e59a65dd6b8695ec16e588ffca6b6285f0fd89c3
zhukovvlad <zhukovvlad@yandex.ru>
2025-09-16 15:24:26 +0300

    12: created-dashboard-page

---
+++ app/lib/deleteVideo.ts
@@ -0,0 +1,24 @@
+'use server'
+
+import { currentUser } from "@clerk/nextjs/server"
+import { prisma } from "./db";
+import { revalidatePath } from "next/cache";
+
+export async function deleteVideo(videoId: string) {
+    try {
+        const user = await currentUser();
+        if (!user) {
+            return null
+        }
+        const userId = user?.id;
+        await prisma.video.delete({
+            where: { videoId: videoId, userId: userId }
+        })
+
+        revalidatePath('/dashboard')
+
+        return { success: true }
+    } catch (error) {
+        return { success: false, error: "Failed to delete video" }
+    }
+}
